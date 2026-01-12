025b8569cf47c8ffdc96c9649d8af2a2b0f66d23
bambarbino <bambarbino10@gmail.com>
2026-01-12 18:42:28 +0300

    пробный pymunk

--- views/test_levels/game_view_test_2.py
+++ views/test_levels/game_view_test_2.py
@@ -15,6 +15,7 @@ class GameView_test_2(GameView_common):
         self.tile_map = Textures.tile_map_test_2
         self.walls_list = self.tile_map.sprite_lists['Walls']
         self.reborn_point_list = self.tile_map.sprite_lists['Reborn_point']
+        self.hook_points_list = self.tile_map.sprite_lists['Hook_points']
 
         self.hero = Hero(tile_map=self.tile_map)
         self.reborn_point = self.reborn_point_list[0].position
@@ -28,8 +29,15 @@ class GameView_test_2(GameView_common):
             gravity_constant=GRAVITY,
             walls=self.walls_list,
         )
+        self.hook_engine = arcade.PymunkPhysicsEngine(gravity=(1, 900))
+        self.hook_engine.add_sprite(self.hero,
+                                    mass=1.0,
+                                    body_type=arcade.PymunkPhysicsEngine.DYNAMIC)
+
         self.hero.engine = self.engine
         self.hero.double_jump = True
+        self.hero.hook_engine = self.hook_engine
 
     def on_draw(self):
         super().on_draw()
+        self.hook_points_list.draw()
